c8896bac1de661c8ae5e260341961ad30e99ad82
Oliver Kowalke <oliver.kowalke@gmail.com>
2024-06-21 17:02:16 +0200

    use dotenv

--- src/api.py
+++ src/api.py
@@ -1,9 +1,14 @@
 import os
+from dotenv import load_dotenv, find_dotenv
 from openai import OpenAI
 
-with open("src/.env") as env:
-    for line in env:
-        key, value = line.strip().split("=")
-        os.environ[key] = value
-
+env_path = find_dotenv()
+load_dotenv(dotenv_path=env_path, verbose=True)
 client = OpenAI(api_key=os.environ["API_KEY"])
+
+
+def get_embedding(text, model):
+    text = text.replace('\n', ' ')
+    return client.embeddings.create(
+            input=[text],
+            model=model).data[0].embedding
